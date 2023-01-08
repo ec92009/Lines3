64dc5e60c6f11f358c0901a0da7462b573f41ba1
Elie Cohen <ec92009@gmail.com>
2023-01-08 11:32:10 +0000

    Initial Commit

---
+++ Lines3/Lines3App.swift
@@ -0,0 +1,17 @@
+//
+//  Lines3App.swift
+//  Lines3
+//
+//  Created by Elie Cohen on 1/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Lines3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
